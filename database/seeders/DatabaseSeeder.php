a56df3773276ed3e46997c4bd1e2178dbc7cb932
China T. Bea <chibea@my.cspc.edu.ph>
2024-10-26 11:57:54 +0800

    added user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        
+        User::create([
+            'firstname' => 'John',
+            'middlename' => 'Az',
+            'lastname' => 'Doe',
+            'email' => 'admin1@gmail.com',
+            'password' => bcrypt('s21010795'), // Use bcrypt for password hashing
+            'role' => 'teacher',
+            'birthdate' => '1990-01-01',
+            'sex' => 'male',
+            'year' => '3rd Year',
+            'section' => 'A',
+            'contact_no' => '09123456789',
+            'emergency_contact' => '09123456789',
+            'address' => '123 Main St, City, Country',
         ]);
+
+        User::create([
+            'firstname' => 'John',
+            'middlename' => 'Del',
+            'lastname' => 'Crus',
+            'email' => 'admin2@gmail.com',
+            'password' => bcrypt('s21010795'),
+            'role' => 'student',
+            'birthdate' => '2000-02-02',
+            'sex' => 'female',
+            'year' => '2nd Year',
+            'section' => 'B',
+            'contact_no' => '09876543210',
+            'emergency_contact' => '09876543210',
+            'address' => '456 Another St, City, Country',
+        ]);
+        
+
+
+
+
+
     }
 }
+
