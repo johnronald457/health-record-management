20a6334c8c8d321c524a2702a05a32aa8a87f9c3
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-10-26 12:55:28 +0800

    Added middleware based on user role routes

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 
-
-Route::get('/', function () {
-    return view('auth.login');
-})->middleware(RedirectIfAuthenticated::class);
-
 // Auth routes
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', function () {
-     return view('admin.index');
-})->middleware('auth');
-
-
-Route::get('/hello', function () {
-    return view('admin.index');
+// Doctor routes
+Route::middleware('role:doctor')->group(function () {
+    Route::get('/doctor-dashboard', function () {
+        return view('admin.index');
+    });
 });
+
+// Nurse routes
+Route::middleware('role:nurse')->group(function () {
+    Route::get('/nurse-dashboard', function () {
+        return view('admin.index');
+    });
+});
+
+// Routes accessible by both teacher and student
+Route::middleware('role:teacher,student')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    });
+});
+
+// RedirectIfAuthenticated
+Route::get('/', function () {
+    return view('auth.login');
+})->middleware(RedirectIfAuthenticated::class);
