d5f068a6bcd172cc4f4f7a31a3088095184db5e6
China T. Bea <chibea@my.cspc.edu.ph>
2024-10-28 18:23:59 +0800

    added treatments migration table

---
+++ database/migrations/2024_10_26_025422_create_treatments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('health_assessment_id')->constrained('health_assessments')->onDelete('cascade');
+            $table->text('interpretation_comments');
+            $table->text('recommendations');
+            $table->text('prescriptions');
+            $table->text('result_summary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatments');
+    }
+};
