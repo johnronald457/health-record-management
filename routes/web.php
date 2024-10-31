e4c4be6169f909bba9763fb80dccf7dd3dec6b14
China T. Bea <chibea@my.cspc.edu.ph>
2024-10-31 20:23:53 +0800

    fsdfdf

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,7 @@ Route::middleware('auth')->group(function () {
         //Dahboard routes
         Route::get('/doctor-dashboard', function () {
             return view('admin.index');
-<<<<<<< Updated upstream
         })->name('doctor.index');
-=======
-        })->name('admin.dashboard');
->>>>>>> Stashed changes
         //Student management routes
         Route::get('/patients', [PatientController::class, 'index'])->name('admin.patient.index');
         Route::get('/patients/{id}', [PatientController::class, 'show'])->name('patients.show');
