859dbef9bac0d5cd5a2d16660f2709fde9926319
China T. Bea <chibea@my.cspc.edu.ph>
2024-10-31 19:57:01 +0800

    update InfoController

--- app/Http/Controllers/Patient/InfoController.php
+++ app/Http/Controllers/Patient/InfoController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\HealthAssessment;
+use Carbon\Carbon;
+
 
 
 class InfoController extends Controller
