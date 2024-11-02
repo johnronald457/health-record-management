e9de9000ed4d841bd68b6a7f0b653c6917df82d6
China T. Bea <chibea@my.cspc.edu.ph>
2024-11-02 17:26:44 +0800

    added health-record

---
+++ resources/views/patient/health-record.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@section('content')
+<div class="shadow mb-4 w-full p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> 
+</div>
+@endsection
