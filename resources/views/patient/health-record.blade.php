0d11b5c7d737866dc95e4fe357286d33831375fb
China T. Bea <chibea@my.cspc.edu.ph>
2024-11-02 18:42:56 +0800

    update health-record.blade.php

--- resources/views/patient/health-record.blade.php
+++ resources/views/patient/health-record.blade.php
@@ -7,6 +7,38 @@
             <div class="col-md-12 mx-auto">
                 <div class="card">
                     <div class="card-body">
+
+                                
+                        <div class="table">
+                            <table class="table table-hover" id="myTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr class="table-light ">
+                                        <!-- <th>Patient ID</th> -->
+                                        <th>Name</th>
+                                        <th>Individual</th>
+                                        <th>Email</th>
+                                        <th>Address</th>
+                                        <th>Contact #</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                        <tr onclick="window.location='{{ route('patient.health-record-history') }}';" style="cursor: pointer;">
+                                            <td>China Bea</td>
+                                            <td>Student</td>
+                                            <td>chinabea@gmail.com</td>
+                                            <td>Calamba Laguna</td>
+                                            <td>09968537599</td>
+                                        </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                 
+
+
+
                         
                     </div>
                 </div>
@@ -15,3 +47,5 @@
     </div> 
 </div>
 @endsection
+
+
