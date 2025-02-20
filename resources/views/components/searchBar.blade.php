420cede7597b54349acaf4946c7ed19554551f4e
karlmikelpecson <karlmikelpecson13@gmail.com>
2025-02-20 20:06:37 +0800

    create components/searchBar file

---
+++ resources/views/components/searchBar.blade.php
@@ -0,0 +1,6 @@
+<div class="mb-4">
+    <div class="input-group">
+        <input type="text" id="searchInput" class="form-control" placeholder="{{ $placeholder ?? 'Search...' }}"
+            autocomplete="off">
+    </div>
+</div>
