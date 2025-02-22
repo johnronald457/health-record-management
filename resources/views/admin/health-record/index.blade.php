2fb87b0b03a905af760b5b716b862a72766db0f7
karlmikelpecson <karlmikelpecson13@gmail.com>
2025-02-22 23:13:09 +0800

    refactor table-data, add total patients and search input

--- resources/views/admin/health-record/index.blade.php
+++ resources/views/admin/health-record/index.blade.php
@@ -1,44 +1,60 @@
 @extends('layout.app')
 
 @section('content')
-<div class="shadow mb-4 w-full p-md-5">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mx-auto">
-                <div class="card">
-                    <div class="card-body">
+    <div class="shadow mb-4 w-full p-md-5">
+        <div class="container">
+            <div class="row">
+                <div class="col mr-0">
+                    <h1 class="display-6 fw-bolder text-uppercase">Health Record</h1>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="d-flex align-items-center">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                <strong>Total:</strong>
+                            </div>
+                            <div class="h5 ms-1 mb-1.5 font-weight-bold text-gray-800" id="patientCount">
+                                {{ $patients->count() }}</div>
+                        </div>
+                        {{-- Search bar --}}
+                        <x-searchBar placeholder="Search patients..." />
+                    </div>
 
-                                
-                        <div class="table table-responsive">
-                            <table class="table table-hover" id="myTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr class="table-light ">
-                                        <!-- <th>Patient ID</th> -->
-                                        <th>Name</th>
-                                        <th>Individual</th>
-                                        <th>Email</th>
-                                        <th>Address</th>
-                                        <th>Contact #</th>
+                </div>
 
+                <div class="card-body">
+
+
+                    <div class="table table-responsive">
+                        <table class="table table-hover" id="myTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr class="table-light ">
+                                    <!-- <th>Patient ID</th> -->
+                                    <th>Name</th>
+                                    <th>Individual</th>
+                                    <th>Email</th>
+                                    <th>Address</th>
+                                    <th>Contact #</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($patients as $patient)
+                                    <tr onclick="window.location='{{ route('patient.health-record-history.show', $patient->id) }}';"
+                                        style="cursor: pointer;">
+                                        <td>{{ $patient->firstname }} {{ $patient->lastname }}</td>
+                                        <td>{{ ucfirst($patient->role) }}</td>
+                                        <td>{{ $patient->email }}</td>
+                                        <td>{{ $patient->address }}</td>
+                                        <td>{{ $patient->contact_no }}</td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                        <tr onclick="window.location='{{ route('patient.health-record-historyy') }}';" style="cursor: pointer;">
-                                            <td>China Bea</td>
-                                            <td>Student</td>
-                                            <td>chinabea@gmail.com</td>
-                                            <td>Calamba Laguna</td>
-                                            <td>09968537599</td>
-                                        </tr>
-                                </tbody>
-                            </table>
-                        </div> 
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
-</div>
+    </div>
+    </div>
+    </div>
+    </div>
 @endsection
-
-
