187a7095d0bfe3286f4c6b223a849dd209307043
karlmikelpecson <karlmikelpecson13@gmail.com>
2025-02-28 20:21:32 +0800

    refactor create treatment table schema

--- database/migrations/2024_10_26_025422_create_treatments_table.php
+++ database/migrations/2024_10_26_025422_create_treatments_table.php
@@ -9,20 +9,36 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    // public function up(): void
+    // {
+    //     Schema::create('treatments', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+    //         $table->foreignId('health_assessment_id')->constrained('health_assessments')->onDelete('cascade');
+    //         $table->text('interpretation_comments');
+    //         $table->text('recommendations');
+    //         $table->text('prescriptions');
+    //         $table->text('result_summary');
+    //         $table->timestamps();
+    //     });
+    // }
+
+    public function up()
     {
         Schema::create('treatments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('health_assessment_id')->constrained('health_assessments')->onDelete('cascade');
-            $table->text('interpretation_comments');
-            $table->text('recommendations');
-            $table->text('prescriptions');
-            $table->text('result_summary');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('health_assessment_id');
+            $table->text('interpretation_comments')->nullable();
+            $table->text('recommendations')->nullable();
+            $table->text('prescriptions')->nullable();
+            $table->text('result_summary')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('health_assessment_id')->references('id')->on('health_assessments')->onDelete('cascade');
         });
     }
-
     /**
      * Reverse the migrations.
      */
