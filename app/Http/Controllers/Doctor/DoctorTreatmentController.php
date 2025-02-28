d5ef45360caaa93164d8178c0fed321087b35e7c
karlmikelpecson <karlmikelpecson13@gmail.com>
2025-02-28 20:20:46 +0800

    add search function - Treatment controller

--- app/Http/Controllers/Doctor/DoctorTreatmentController.php
+++ app/Http/Controllers/Doctor/DoctorTreatmentController.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Http\Controllers\Doctor;
+
 use App\Http\Controllers\Controller;
 use App\Models\Treatment;
 use Illuminate\Http\Request;
 use App\Models\HealthAssessment;
 use Illuminate\Support\Facades\Auth;
+
 class DoctorTreatmentController extends Controller
 {
     public function index()
@@ -14,18 +16,26 @@ class DoctorTreatmentController extends Controller
         // whereNotIn('role', ['nurse', 'doctor'])->get();
         return view('admin.treatment.index', compact('treatments'));
     }
+
+    // public function show($id)
+    // {
+    //     $treatment = Treatment::where('id', $id)->first();
+    //     return view('admin.treatment.show', compact('treatment'));
+    // }
+
     public function show($id)
     {
-        $treatment = Treatment::where('id', $id)->first();
+        $treatment = Treatment::with(['user', 'health_assessment'])->findOrFail($id);
+
         return view('admin.treatment.show', compact('treatment'));
     }
-     public function edit($id)
+    public function edit($id)
     {
-            $treatment = Treatment::where('health_assessment_id', $id)->first();
-    
-            return view('admin.treatment.edit-health-assessment', compact('treatment'));
+        $treatment = Treatment::where('health_assessment_id', $id)->first();
+
+        return view('admin.treatment.edit-health-assessment', compact('treatment'));
     }
-        public function update(Request $request, $id)
+    public function update(Request $request, $id)
     {
         $healthAssessment = HealthAssessment::where('id', $id);
 
@@ -48,5 +58,30 @@ class DoctorTreatmentController extends Controller
         $treatment->delete();
         session()->flash('success', 'Treatment deleted successfully.');
         return redirect()->route('admin.treatment.index')->with('success', 'treatment deleted successfully.');
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+
+        $treatments = Treatment::where(function ($query) use ($search) {
+            $query->whereHas('user', function ($q) use ($search) {
+                $q->where('firstname', 'LIKE', "%{$search}%")
+                    ->orWhere('lastname', 'LIKE', "%{$search}%");
+            })
+                ->orWhereHas('health_assessment', function ($q) use ($search) {
+                    $q->where('medical_history', 'LIKE', "%{$search}%")
+                        ->orWhere('medical_conditions', 'LIKE', "%{$search}%")
+                        ->orWhere('blood_pressure', 'LIKE', "%{$search}%")
+                        ->orWhere('symptoms', 'LIKE', "%{$search}%")
+                        ->orWhere('allergies', 'LIKE', "%{$search}%");
+                });
+        })
+            ->when(empty($search), function ($query) {
+                return $query->limit(10);
+            })
+            ->get();
+
+        return response()->json($treatments);
     }
 }
