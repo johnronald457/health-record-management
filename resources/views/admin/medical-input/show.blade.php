c1e68062621630542ca12143e66e004cad62d7ec
karlmikelpecson <karlmikelpecson13@gmail.com>
2025-03-31 16:32:24 +0800

    remove condition and priority level

--- resources/views/admin/medical-input/show.blade.php
+++ resources/views/admin/medical-input/show.blade.php
@@ -25,11 +25,11 @@
                             {{ $medical->patient->firstname }} {{ $medical->patient->middlename }}</p>
                         <p class="card-text"><strong>Medical Type:</strong> {{ ucfirst($medical->request_type) }}</p>
                         <p class="card-text"><strong>Status:</strong> {{ ucfirst($medical->status) }}</p>
-                        <p class="card-text"><strong>Priority:</strong> {{ ucfirst($medical->priority) }}</p>
+                        {{-- <p class="card-text"><strong>Priority:</strong> {{ ucfirst($medical->priority) }}</p> --}}
                         <p class="card-text"><strong>Findings:</strong> {{ $medical->findings ?? 'N/A' }}</p>
                         <p class="card-text"><strong>Test Action Date:</strong>
                             {{ \Carbon\Carbon::parse($medical->test_date)->format('F j, Y') }}</p>
-                        <p class="card-text"><strong>Condition:</strong> {{ $medical->condition }}</p>
+                        {{-- <p class="card-text"><strong>Condition:</strong> {{ $medical->condition }}</p> --}}
                         <p class="card-text"><strong>Comments:</strong> {{ $medical->description ?? 'N/A' }}</p>
                     </div>
                     <!-- Right Side: Patient Picture -->
